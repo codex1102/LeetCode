5f8ab24d9b6c58efc3242fddcf26a3ab4b80d6b7
ujjwal kumar chauhan <ujjwalrsvm@gmail.com>
2026-07-17 15:24:20 +0530

    Time: 7 ms (40.6%), Space: 20.4 MB (61.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,14 +2,13 @@ class Solution:
     def moveZeroes(self, nums):
         result = []
 
-        # Add all non-zero elements
         for num in nums:
             if num != 0:
                 result.append(num)
 
-        # Add zeros
+        
         while len(result) < len(nums):
             result.append(0)
 
-        # Copy back to original array
+        
         nums[:] = result
