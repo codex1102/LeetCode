45cd194d49d4dd2b0789f3ca3ce9bac21013f080
ujjwal kumar chauhan <ujjwalrsvm@gmail.com>
2026-07-27 13:41:29 +0530

    Time: 3 ms (34.01%), Space: 19.4 MB (24.15%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,24 +1,19 @@
 class Solution:
-
-    def squareSum(self, n):
-
-        total = 0
-
-        while n > 0:
-            digit = n % 10
-            total += digit * digit
-            n //= 10
-
-        return total
-
     def isHappy(self, n):
 
-        slow = n
-        fast = self.squareSum(n)
+        visited = set()
 
-        while fast != 1 and slow != fast:
+        while n != 1 and n not in visited:
 
-            slow = self.squareSum(slow)
-            fast = self.squareSum(self.squareSum(fast))
+            visited.add(n)
 
-        return fast == 1
+            total = 0
+
+            while n > 0:
+                digit = n % 10
+                total += digit * digit
+                n //= 10
+
+            n = total
+
+        return n == 1
