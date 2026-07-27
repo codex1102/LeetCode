92620e2f892c9416e2035a1440f8fa8c38564cbc
ujjwal kumar chauhan <ujjwalrsvm@gmail.com>
2026-07-27 10:27:12 +0530

    Time: 54 ms (56.26%), Space: 22.9 MB (7.44%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasCycle(self, head):
+
+        visited = set()
+
+        current = head
+
+        while current:
+
+            if current in visited:
+                return True
+
+            visited.add(current)
+
+            current = current.next
+
+        return False
