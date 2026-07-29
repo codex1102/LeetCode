51a320b34d04281d7790585943d1ed0e790446ec
ujjwal kumar chauhan <ujjwalrsvm@gmail.com>
2026-07-29 13:25:21 +0530

    Time: 71 ms (91.18%), Space: 25.2 MB (50.35%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMaxLength(self, nums):
+
+        prefix_sum = 0
+        max_length = 0
+
+        first_index = {0: -1}
+
+        for i in range(len(nums)):
+
+            if nums[i] == 0:
+                prefix_sum -= 1
+            else:
+                prefix_sum += 1
+
+            if prefix_sum in first_index:
+                max_length = max(max_length, i - first_index[prefix_sum])
+            else:
+                first_index[prefix_sum] = i
+
+        return max_length
