5d046bfdf56251727ba24e9a199ba988d5dd7031
ujjwal kumar chauhan <ujjwalrsvm@gmail.com>
2026-08-01 17:29:26 +0530

    Time: 0 ms (100%), Space: 19.3 MB (95.96%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        ans = -1
+        for i in range (len(nums)):
+            if nums[i] == target:
+                ans = i
+        return ans
+                
+
